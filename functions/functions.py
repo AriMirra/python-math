125139379cc20bb776413bd65ad0ecb04045d49d
arielmirra <ariel.mirra@ing.austral.edu.ar>
2019-09-03 17:00:46 -0300

    add newton function

--- functions/functions.py
+++ functions/functions.py
@@ -1,5 +1,3 @@
-
-
 def bisection(f, a, b, error_level=0.001):
     previous_p = a
     error = error_level + 1
@@ -16,3 +14,39 @@ def bisection(f, a, b, error_level=0.001):
         return p
 
 
+def s(i, cs, xs):
+    s = 0
+    for j in range(i):
+        p = 1
+        for k in range(j):
+            p *= xs[i] - xs[k]
+        s += cs[j] * p
+
+    return s
+
+
+def q(i, xs):
+    p = 1
+    for k in range(i):
+        p *= xs[i] - xs[k]
+    return p
+
+
+def newton_pol(xs, ys):
+    cs = []
+    n = len(xs)
+    for i in range(n):
+        ck = (ys[i] - s(i, cs, xs)) / q(i, xs)
+        cs.append(ck)
+    print(cs)
+    pol_str = ""
+    for i in range(n):
+        pol_str += str(cs[i])
+        for j in range(i):
+            pol_str += "*(x-%s)" % xs[j]
+        if i != n - 1:
+            pol_str += " + "
+    return lambda x: eval(pol_str, {'x': xs})
+
+
+print(newton_pol([1.5, 1, 0.5, 2], [1, 0.5, 1.2, 3]))
